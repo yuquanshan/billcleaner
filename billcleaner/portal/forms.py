3b8ed094cb44dd451d1217db371c686f424b217f
yuquanshan <springshan13@gmail.com>
2017-06-17 18:02:43 -0700

    Add more fields

--- billcleaner/portal/forms.py
+++ billcleaner/portal/forms.py
@@ -11,7 +11,8 @@ class SignUpForm(UserCreationForm):
 class ReportForm(ModelForm):
 	class Meta:
 		model = Peep
-		fields = ['fri', 'frimoney', 'sat', 'satmoney', 'sun', 'sunmoney', 'mon', 'monmoney']
+		fields = ['fri', 'frimoney', 'sat', 'satmoney', 'sun', 'sunmoney', 
+		'mon', 'monmoney', 'sat1', 'sat1money', 'sun1', 'sun1money']
 		labels = {
 			'fri': 'Friday (Bowling)',
 			'frimoney': 'Friday Expense',
@@ -21,10 +22,16 @@ class ReportForm(ModelForm):
 			'sunmoney': 'Sunday Expense',
 			'mon': 'Monday (Santa Barbara etc)',
 			'monmoney': 'Monday Expense',
+			'sat1': 'Saturday (San Jose Night Bar)',
+			'sat1money': 'Another Saturday Expense',
+			'sun1': 'Sunday (San Jose Daytime)',
+			'sun1money': 'Another Sunday Expense',
 		}
 		help_texts = {
 			'fri': 'Click if you participated',
 			'sat': 'Click if you participated',
 			'sun': 'Click if you participated',
 			'mon': 'Click if you participated',
+			'sat1': 'Click if you participated',
+			'sun1': 'Click if you participated',
 		}
